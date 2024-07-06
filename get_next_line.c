7b3cf02c4eb47b99e5de6ac41b6628b1176173a6
Ana Luisa Eberius Nunan <nunan@MacBook-Pro-de-Ana-7.local>
2024-07-06 13:33:41 +0100

    lesgo

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,87 @@
 /*   By: aeberius <aeberius@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 14:17:47 by aeberius          #+#    #+#             */
-/*   Updated: 2024/06/15 14:34:59 by aeberius         ###   ########.fr       */
+/*   Updated: 2024/07/06 13:22:15 by aeberius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
-{
-	static char*	buffer_file_finaly;
+static char	*read_buffer(int fd, char *result, char *buffer);
+static char	*copy_until_newline(char *str);
+static void	strcpy_from_newline(char *str);
 
+static char	*read_buffer(int fd, char *result, char *buffer)
+{
+	int	bytes_read;
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		result = ft_strjoin(result, copy_until_newline(buffer));
+		strcpy_from_newline(buffer);
+		if (ft_strchr(result, '\n'))
+			return (result);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
+	if (bytes_read == -1)
+	{
+		if (result)
+			free(result);
+		return (NULL);
+	}
+	return (result);
+}
+
+static char	*copy_until_newline(char *str)
+{
+	int		i;
+	char	*result;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	result = (char *)malloc(sizeof(char) * i + 1);
+	result[i] = 0;
+	while (i-- > 0)
+		result[i] = str[i];
+	return (result);
+}
+
+static void	strcpy_from_newline(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	j = 0;
+	while (str[i])
+		str[j++] = str[i++];
+	while (str[j])
+		str[j++] = 0;
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		*result;
+
+	if (fd < 0)
+		return (NULL);
+	result = NULL;
+	if (buffer[0])
+	{
+		result = copy_until_newline(buffer);
+		strcpy_from_newline(buffer);
+		if (ft_strchr(result, '\n'))
+			return (result);
+	}
+	result = read_buffer(fd, result, buffer);
+	return (result);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,25 @@
 /*   By: aeberius <aeberius@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 14:19:55 by aeberius          #+#    #+#             */
-/*   Updated: 2024/06/15 14:25:23 by aeberius         ###   ########.fr       */
+/*   Updated: 2024/07/06 13:18:32 by aeberius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
